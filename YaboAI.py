d99ff66ffd13674439a187e16b010d3911b9e69c
Jason <jasoncardinale19@gmail.com>
2023-04-19 01:04:39 -0400

    tracking driver standings and other stuff

--- YaboAI.py
+++ YaboAI.py
@@ -1,17 +1,46 @@
 import ac
 import acsys
 from third_party.sim_info import *
+import random
+
+# Events
+OVERTAKE = "overtake"
+COLLISION = "collision"
+FASTEST_LAP = "fastest_lap"
+FASTEST_SECTOR = "fastest_sector"
+SHORT_INTERVAL = "short_interval"
+DNF = "dnf"
 
 # Global constants
 appName = "YaboAI"
 simInfo = SimInfo()
-promptSuffix = "In the style of Jeremy Clarkson working as an F1 commentator"
+focusTimeMin = 3
+focusTimeMax = 10
+
+# We don't want to be reporting events that happened too long ago
+# so this will apply weights to events according to how likely 
+# they are to be discarded from the queue if we are currently reporting
+# and there are more events later in the queue
+eventPriority = { OVERTAKE: 0.2, COLLISION: 0.2, FASTEST_LAP: 0.4, FASTEST_SECTOR: 0.6, SHORT_INTERVAL: 0.5, DNF: 0.1 }
 
 # Global variables
 lastUpdateTime = 0
-eventQueue = []
 isCommentating = False
 driverCount = 32
+carInFocus = 0
+raceMode = -1
+
+eventQueue = []
+driverList = []
+
+stateCurrent: any = None
+statePrevious: any = None
+
+
+class RaceState:
+    def __init__(self, standing):
+        self.standing = standing
+        
 
 class Driver:
     def __init__(self, id):
@@ -23,13 +52,23 @@ class Driver:
         self.bestLap = ac.getCarState(id, acsys.CS.BestLap)
         self.lastLap = ac.getCarState(id, acsys.CS.LastLap)
         self.lapCount = ac.getCarState(id, acsys.CS.LapCount)
-        self.speedMS = ac.getCarState(id, acsys.CS.SpeedMS)
+        self.speedKMH = ac.getCarState(id, acsys.CS.SpeedKMH)
         self.lapDistance = ac.getCarState(id, acsys.CS.NormalizedSplinePosition)
         self.distance = ac.getCarState(id, acsys.CS.LapCount) + ac.getCarState(id, acsys.CS.NormalizedSplinePosition)
         self.compound = ac.getCarTyreCompound(id)
         self.inPit = ac.isCarInPitline(id)
         self.currentSector = -1
         self.pitStops = 0
+        self.runningPosition = 0
+
+
+class Event:
+    def __init__(self, type, time, drivers, parameters):
+        self.type = type
+        self.time = time
+        self.drivers = drivers
+        self.parameters = parameters
+
 
 def acMain(ac_version):
     global appWindow, driverCount
@@ -40,32 +79,77 @@ def acMain(ac_version):
 
     ac.addRenderCallback(appWindow, appGL)
 
-    ac.log("Hello, Assetto Corsa application world!")
-    ac.console("Hello, Assetto Corsa console!")
     driverCount = ac.getCarsCount()
+    for id in range(driverCount):
+        driverList.append(Driver(id))
 
     return appName
+
 
 def appGL(deltaT):
     pass
 
+
+def eventPriority():
+    pass
+
+def resetState():
+    global lastUpdateTime, stateCurrent, statePrevious
+    lastUpdateTime = 0
+    statePrevious = stateCurrent
+
 def acUpdate(deltaT):
-    global lastUpdateTime, eventQueue, driverCount
+    global lastUpdateTime, eventQueue, driverCount, carInFocus, raceMode, statePrevious, stateCurrent
 
     lastUpdateTime += deltaT
-
     if lastUpdateTime < 1:
         return
 
-    lastUpdateTime = 0
-    ac.log(simInfo.static.playerName)
-    ac.log(simInfo.static.playerNick)
+    if raceMode == 2:
+        driverList.sort(key=lambda driver: driver.distance, reverse=True)
+    else:
+        for driver in driverList:
+            if driver.bestLap == 0:
+                driver.bestLap = 9999999
+        driverList.sort(key=lambda driver: driver.bestLap)
+    
+    standing = [d for d in driverList if d.connected]
+
+    stateCurrent = RaceState(standing)
+
+    if statePrevious == None:
+        resetState()
+        return
+
+    if stateCurrent.standing != statePrevious.standing:
+        max_len = max(len(statePrevious.standing), len(stateCurrent.standing))
+        for i in range(max_len):
+            if stateCurrent.standing[i] != statePrevious.standing[i]:
+                pass
+
+    if not eventQueue:
+        resetState()
+        return
+
+    if isCommentating:
+        # use probability to determine if the next event in queue should be discarded
+        pass
+    else:
+        event = eventQueue.pop(0)
+        prompt = generatePrompt(event)
+
+    resetState()
+
+
 
 def generatePrompt(event):
+    prompt = f"" 
     pass
+
 
 def enhanceText(prompt):
     pass
+
 
 def textToSpeech(text):
     pass
