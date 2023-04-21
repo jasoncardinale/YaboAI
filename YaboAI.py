9921a943690e75ace3a579a43a5f190dc0cea9a1
Jason <jasoncardinale19@gmail.com>
2023-04-21 01:48:43 -0400

    increased state tracking, creating pit events

--- YaboAI.py
+++ YaboAI.py
@@ -12,6 +12,8 @@ FASTEST_SECTOR = "fastest_sector"
 SHORT_INTERVAL = "short_interval"
 DNF = "dnf"
 LONG_STINT = "long_stint"
+ABNORMAL_PIT = "abnormal_pit"
+ENTERED_PIT = "entered_pit"
 
 # Global constants
 appName = "YaboAI"
@@ -31,7 +33,6 @@ isCommentating = False
 driverCount = 32
 sectorCount = 0
 carInFocus = 0
-raceMode = -1
 
 eventQueue = []
 drivers = []
@@ -40,10 +41,12 @@ stateCurrent: any = None
 statePrevious: any = None
 
 class RaceState:
-    def __init__(self, standing, fastestLap, fastestSectors, intervals):
+    def __init__(self, drivers, standing, fastestLap, intervals, raceMode):
+        self.drivers = drivers
         self.standing = standing
         self.fastestLap = fastestLap
         self.intervals = intervals
+        self.raceMode = raceMode
 
 class Driver:
     def __init__(self, id):
@@ -55,7 +58,7 @@ class Driver:
 
         # Continuously updated
         self.connected = ac.isConnected(id)
-        self.bestLap = ac.getCarState(id, acsys.CS.BestLap)
+        self.bestLaps = ac.getCarState(id, acsys.CS.BestLap)
         self.lastLap = ac.getCarState(id, acsys.CS.LastLap)
         self.lapCount = ac.getCarState(id, acsys.CS.LapCount)
         self.speedKMH = ac.getCarState(id, acsys.CS.SpeedKMH)
@@ -71,7 +74,7 @@ class Driver:
 
     def update(self):
         self.connected = ac.isConnected(id)
-        self.bestLap = ac.getCarState(id, acsys.CS.BestLap)
+        self.bestLaps = ac.getCarState(id, acsys.CS.BestLap)
         self.lastLap = ac.getCarState(id, acsys.CS.LastLap)
         self.lapCount = ac.getCarState(id, acsys.CS.LapCount)
         self.speedKMH = ac.getCarState(id, acsys.CS.SpeedKMH)
@@ -89,7 +92,7 @@ class Event:
 
 
 def acMain(ac_version):
-    global appWindow, driverCount
+    global appWindow, driverCount, stateCurrent, statePrevious
 
     appWindow = ac.newApp(appName)
     ac.setTitle(appWindow, appName)
@@ -101,6 +104,9 @@ def acMain(ac_version):
     for id in range(driverCount):
         drivers.append(Driver(id))
 
+    standing = [d.carId for d in drivers if d.connected]
+    intervals = [0 for d in standing]
+    stateCurrent, statePrevious = RaceState(drivers, standing, 9999999, [intervals], -1)
     return appName
 
 
@@ -117,31 +123,33 @@ def reset():
     statePrevious = stateCurrent
 
 def acUpdate(deltaT):
-    global lastUpdateTime, eventQueue, driverCount, carInFocus, raceMode, statePrevious, stateCurrent, drivers
+    global lastUpdateTime, eventQueue, driverCount, carInFocus, statePrevious, stateCurrent, drivers
 
     lastUpdateTime += deltaT
     if lastUpdateTime < 1:
         return
 
     # Get current state
+    # Race mode
+    stateCurrent.raceMode = simInfo.graphics.session
+
     # Update drivers
-    for driver in drivers:
+    for driver in stateCurrent.drivers:
         driver.update()
 
     # Determine current standings
-    if raceMode == 2:
+    if stateCurrent.raceMode == 2:
         drivers.sort(key=lambda driver: driver.distance, reverse=True)
     else:
         for driver in drivers:
             if driver.bestLap == 0:
                 driver.bestLap = 9999999
         drivers.sort(key=lambda driver: driver.bestLap)
-
-    standing = [d for d in drivers if d.connected]
+    standing = [d.carId for d in drivers if d.connected]
 
     # Determine the fastest lap
     fastestLap = statePrevious.bestLap
-    for driver in drivers:
+    for driver in stateCurrent.drivers:
         if driver.bestLap < fastestLap[0]:
             fastestLap = (driver.bestLap, driver)
 
@@ -160,6 +168,21 @@ def acUpdate(deltaT):
         for i in range(max_len):
             if stateCurrent.standing[i] != statePrevious.standing[i]:
                 pass
+
+    # Compare pit times
+    for driverCurrent in stateCurrent.drivers:
+        driverPrevious = statePrevious[[i for i, d in enumerate(statePrevious.drivers) if d.carId == driverCurrent.carId][0]]
+        # Driver has entered the pits
+        if not driverPrevious.inPit and driverCurrent.inPit:
+            driverCurrent.lastPitStart = datetime.datetime.now()
+            eventQueue.append(Event(ENTERED_PIT, datetime.datetime.now(), driverCurrent))
+        # Driver has exited the pits
+        if driverPrevious.inPit and not driverCurrent.inPit:
+            driverCurrent.lastPitEnd = datetime.datetime.now()
+            pitLength = (driverCurrent.lastPitEnd - driverCurrent.lastPitStart).total_seconds()
+            if pitLength > 60 or pitLength < 30:
+                eventQueue.append(Event(ABNORMAL_PIT, datetime.datetime.now(), driverCurrent, { "pit_length": pitLength }))
+
 
     if not eventQueue:
         reset()
