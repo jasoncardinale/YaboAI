bdbc3f0ed3367cf9b757cb11e5f89327e0fd471d
jasoncardinale <jasoncardinale19@gmail.com>
2024-09-07 10:35:02 -0400

    added folder structure + ollama support

---
+++ llm/services.py
@@ -0,0 +1,24 @@
+import http.client
+import json
+
+
+def chat_completion(prompt: str) -> str | None:
+    conn = http.client.HTTPConnection("localhost", 11434)
+    headers = {"Content-type": "application/json"}
+    payload = json.dumps(
+        {
+            "model": "llama3.1:8b",
+            "prompt": prompt,
+            "stream": False,
+        }
+    )
+
+    conn.request("POST", "/api/generate", payload, headers)
+    response = conn.getresponse()
+
+    if response.status == 200:
+        response_data = response.read().decode()
+        return json.loads(response_data)["response"].strip()
+    else:
+        print(f"Error: {response.status}")
+        return None
